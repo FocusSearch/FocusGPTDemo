7418c3061385076d32a9187596b5e4d90e2cf45f
shaojk-datafocus <shaojk@openscanner.cn>
2025-02-26 09:41:06 +0800

    完成前后端合并脚本开发

--- start.bat
+++ start.bat
@@ -1,17 +1,17 @@
 cd web
-@REM call npm install
-@REM call npm run build
+call npm install
+call npm run build
 cd ..
 copy "web\dist\public\index.html" "server\templates\index.html"
 
 for %%a in (css, js, img, datafocus-chart) do (
     mkdir "server\static\%%a"
     del /f /s /q "server\static\%%a\*.*"
-    xcopy /Y "web\dist\%%a" "server\static\%%a"
+    xcopy /s /y "web\dist\%%a" "server\static\%%a"
 )
-@REM python -m pip install -r server/requirements.txt
+python -m pip install -r server/requirements.txt
 
-set DF_URL="https://cloud001.datafocus.ai"
-set DF_APP_TOKEN="abcdef"
+set DF_URL=https://cloud001.datafocus.ai
+set DF_APP_TOKEN=********
 
 python server/app.py
